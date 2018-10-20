3aa9a958b5db0539d5015787259865dcaf745314
bangseonho <roomseonho@gmail.com>
2018-10-20 14:38:56 +0900

    start

---
+++ app/assets/javascripts/cable.js
@@ -0,0 +1,26 @@
+// Action Cable provides the framework to deal with WebSockets in Rails.
+// You can generate new channels where WebSocket features live using the rails generate channel command.
+//
+//= require action_cable
+//= require_self
+//= require_tree ./channels
+
+(function() {
+  this.App || (this.App = {});
+
+  App.cable = ActionCable.createConsumer();
+
+}).call(this);
+
+$(function() {
+    $( "#Datepicker" ).datepicker({
+    	numberOfMonths: [1,2],
+    	dateFormat: "yy.mm.dd",
+    	nextText: '다음 달',
+        prevText: '이전 달',
+        dayNames: ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+        dayNamesMin: ['월', '화', '수', '목', '금', '토', '일'], 
+        monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
+        monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월']
+    });
+});
