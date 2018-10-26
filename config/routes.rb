510d86f13621f7874a85dfdf611297fb635fe1e0
bangseonho <37283474+bangseonho@users.noreply.github.com>
2018-10-26 22:18:01 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'front_pages#home'
 
+  get '/front_pages/info' => 'front_pages#info'
+  get '/front_pages/index' => 'front_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
