4e59717eab36e52353606619a3338bc612da1a74
bangseonho <37283474+bangseonho@users.noreply.github.com>
2018-10-31 20:44:38 +0900

    Update

--- app/controllers/front_pages_controller.rb
+++ app/controllers/front_pages_controller.rb
@@ -15,11 +15,12 @@ class FrontPagesController < ApplicationController
   	flight.arrivalday = params[:arrivalday]
   	flight.save
 
-  	url = 'http://openapi.airport.co.kr/service/rest/FlightScheduleList/getIflightScheduleList?ServiceKey=0WseDqrRGlPch1V5tVepuGfT4L%2FKTND3IQyyvCV%2FZEvqR5ltzam5jBIIHhZJZt4p0DGRrAFThw7iFaD7q9mF4A%3D%3D&schDate=<% flight.departureday %>&schDeptCityCode=<% flight.departure %>&schArrvCityCode=<% flight.arrival %>'
-  	headers = { :params => { CGI::escape('numOfRows') => '10'} }
-		@response = RestClient::Request.execute :method => 'GET', :url => url , :headers => headers
-		@response_xml = @response.body
-  	#redirect_to '/front_pages/info'
+    # Request ariport API #
+  	# url = 'http://openapi.airport.co.kr/service/rest/FlightScheduleList/getIflightScheduleList?ServiceKey=0WseDqrRGlPch1V5tVepuGfT4L%2FKTND3IQyyvCV%2FZEvqR5ltzam5jBIIHhZJZt4p0DGRrAFThw7iFaD7q9mF4A%3D%3D&schDate=<% flight.departureday %>&schDeptCityCode=<% flight.departure %>&schArrvCityCode=<% flight.arrival %>'
+  	# headers = { :params => { CGI::escape('numOfRows') => '10'} }
+		# @response = RestClient::Request.execute :method => 'GET', :url => url , :headers => headers
+		# @response_xml = @response.body
+  	# redirect_to '/front_pages/info'
   end
   
   def index
